131b9b3bc274969edbf847722c77c973e4ccf8b2
weaver010 <abdelrahmannagaty839@gmail.com>
2023-12-19 06:27:11 -0800

    hi

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -8,13 +8,13 @@ void bubble_sort(int *array, size_t size)
 {
 unsigned int i = 0, j = 0;
 int t = 0;
-if (array == NULL || size < 2)
+if (size < 2)
 {
 return;
 }
 for (i = 0; i < size; i++)
 {
-for (j = 0; j < size; j++)
+for (j = 0; j <= size; j++)
 {
 if (array[j] > array[j + 1])
 {
